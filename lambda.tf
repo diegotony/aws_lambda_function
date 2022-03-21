05a8ae3580999b35a2a50bb64cce2aa7723bf6a5
Diego Castillo <diegotony1396@gmail.com>
2022-03-20 22:59:28 -0500

    test pipeline

--- lambda.tf
+++ lambda.tf
@@ -1,9 +1,9 @@
 
 locals {
   environment_variables = {}
-  tags = {template = "tf-modules", service = "lambda"}
-  layers = []
-  description = ""
+  tags                  = { template = "tf-modules", service = "lambda" }
+  layers                = []
+  description           = ""
 }
 
 resource "aws_lambda_function" "this" {
@@ -12,16 +12,16 @@ resource "aws_lambda_function" "this" {
   role          = aws_iam_role.lambda_exec.arn
   runtime       = var.runtime
   handler       = var.handler
-  layers        = var.layers == tolist([""])  ? merge(var.layers,local.layers) : null 
-  description   = format("%s | %s | %s",var.description, local.description,"${var.function_name}")
+  layers        = var.layers == tolist([""]) ? merge(var.layers, local.layers) : null
+  description   = format("%s | %s | %s", var.description, local.description, "${var.function_name}")
   architectures = var.architectures
-  image_uri =  var.image_uri == null ? null : var.image_uri
-  memory_size = var.memory_size
-  package_type = var.package_type
-  timeout = var.timeout
+  image_uri     = var.image_uri == null ? null : var.image_uri
+  memory_size   = var.memory_size
+  package_type  = var.package_type
+  timeout       = var.timeout
 
   environment {
-    variables = var.environment_variables == tolist([""]) ? merge(var.environment_variables,local.environment_variables) : null 
+    variables = var.environment_variables == tolist([""]) ? merge(var.environment_variables, local.environment_variables) : null
   }
 
   tags = merge(var.tags, local.tags)
--- zip_file.tf
+++ zip_file.tf
@@ -1,5 +1,5 @@
 data "archive_file" "zip" {
-  type = "zip"
+  type        = "zip"
   source_dir  = var.source_dir
-  output_path =  "${var.source_dir}/${var.function_name}.zip"
+  output_path = "${var.source_dir}/${var.function_name}.zip"
 }
